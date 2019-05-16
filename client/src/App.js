e287750fac690926101215f74151fdd0076c3a77
Sarah Maples <sarah.e.maples@gmail.com>
2019-05-15 21:17:12 -0500

    working version of mvp with absolutely no styling whatsoever

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,65 @@
-import React, { Component } from 'react'
-import UsernameForm from './components/UsernameForm.js'
-import ChatScreen from './components/ChatScreen.js'
+import React from 'react';
+import Chatkit from '@pusher/chatkit-client';
+import MessageList from './components/MessageList';
+import SendMessageForm from './components/SendMessageForm';
+import './App.css';
 
-class App extends Component {
-  constructor() {
-    super()
+class App extends React.Component{
+
+  constructor(){
+    super();
     this.state = {
-      currentScreen: 'WhatIsYourUsernameScreen',
-      currentUsername: ''
+      messages: []
     }
-    this.onUsernameSubmitted = this.onUsernameSubmitted.bind(this)
+    this.sendMessage = this.sendMessage.bind(this)
   }
-  onUsernameSubmitted(username) {
-    fetch('http://localhost:3001/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ username }),
+
+  componentDidMount(){
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator: 'v1:us1:758e334a-5a1d-4660-8590-24de4fb4637f',
+      userId: "sarah",
+      tokenProvider: new Chatkit.TokenProvider({
+        url: "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/758e334a-5a1d-4660-8590-24de4fb4637f/token"
+      })
+    });
+   
+    chatManager.connect()
+    .then(currentUser => {
+      this.currentUser = currentUser
+      console.log(currentUser);
+      this.currentUser.subscribeToRoom({
+        roomId: '19422811',
+        hooks: {
+          onMessage: message => {
+            console.log(message.senderId, ': ', message.text);
+            this.setState({
+              messages: [...this.state.messages, message]
+            })
+          }
+        }
+      })
     })
-      .then(response => {
-        console.log('success')
-        this.setState({
-          currentUsername: username,
-          currentScreen: 'ChatScreen'
-        })
-      })
-      .catch(error => {
-        console.log(error)
-      })
+
   }
+
+  sendMessage(text) {
+    console.log(this.currentUser)
+    this.currentUser.sendMessage({
+      text,
+      roomId: '19422811' 
+    });
+  }
+
   render() {
-    if (this.state.currentScreen === 'WhatIsYourUsernameScreen') {
-      return <UsernameForm onSubmit={this.onUsernameSubmitted} />
-    } else if (this.state.currentScreen === 'ChatScreen') {
-      return <ChatScreen currentUsername={this.state.currentUsername} />
-    }
+    console.log('this.state.messages', this.state.messages);
+    return (
+      <div className="App">
+          <MessageList messages={this.state.messages}/>
+          <SendMessageForm sendMessage={this.sendMessage}/>
+      </div>
+    )
   }
+
 }
 
-  export default App
+export default App;

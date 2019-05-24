bf2dd2349e6685b28f02890d3a173bc9c27eb684
travishill1 <travishill1@gmail.com>
2019-05-24 01:48:58 -0500

    Added pic and options to MiniProfile; no functionality yet

--- client/src/components/MiniProfile.js
+++ client/src/components/MiniProfile.js
@@ -1,16 +1,15 @@
 import React from 'react'
 
 class MiniProfile extends React.Component {
-    render () {
+    render() {
         const currentUser = this.props.currentUser
         return (
             <div className="mini-profile">
                 <ul>
-                <h4>Your Name:</h4>
-               <li> {currentUser} </li>
-               {console.log(this)}
-               {console.log(currentUser)}
-               {console.log(this.props.currentUser)}
+                    <li><img className="mini-profile-pic" src="https://i.pinimg.com/originals/6a/4d/b0/6a4db06a551690903d497fee1cb81016.jpg"></img></li>
+                    <li> <h3> {currentUser} </h3></li>
+                    <li> <a><img className="mini-profile-options" src="https://cdn4.iconfinder.com/data/icons/software-menu-icons/256/SoftwareIcons-23-512.png"></img></a> </li>
+                    {console.log(currentUser)}
                 </ul>
             </div>
         )

434198187d9af3fd24c1b137e76bfa2114343736
travishill1 <travishill1@gmail.com>
2019-05-30 22:12:49 -0500

    alt tags added to avatars and options wheel to get rid of annoying errors

--- client/src/components/MiniProfile.js
+++ client/src/components/MiniProfile.js
@@ -6,9 +6,9 @@ class MiniProfile extends React.Component {
         return (
             <div className="mini-profile">
                 <ul>
-                    <li><img className="mini-profile-pic" src={currentUser.avatar_url}></img></li>
+                    <li><img className="mini-profile-pic" alt={currentUser.id} src={currentUser.avatar_url}></img></li>
                     <li> <h3> {currentUser.id} </h3></li>
-                    <li> <a><img className="mini-profile-options" src="https://cdn4.iconfinder.com/data/icons/software-menu-icons/256/SoftwareIcons-23-512.png"></img></a> </li>
+                    <li> <a><img className="mini-profile-options" alt="options-wheel" src="https://cdn4.iconfinder.com/data/icons/software-menu-icons/256/SoftwareIcons-23-512.png"></img></a> </li>
                     {/* {console.log(currentUser)} */}
                 </ul>
             </div>
